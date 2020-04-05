cc408139949ce7ce4c2af6fdaa86d792c7b59e13
PDA-Khacer <59501767_PDA_Khacer@users.noreply.github.com>
2020-04-05 16:57:37 +0700

    contruction page

--- resource/js/scripts.js
+++ resource/js/scripts.js
@@ -1,10 +1,9 @@
-funtion openSearch(evt, name) {
+function openSearch(evt, name) {
     var i, tabContent, tablinks;
     tabContent = document.getElementsByClassName("tabContent");
     for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
     }
-
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace("active", "");
