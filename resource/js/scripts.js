66a26b93d1123fdd86751a3c35f80406afd82c3d
PDA-Khacer <59501767_PDA_Khacer@users.noreply.github.com>
2020-04-05 11:38:23 +0700

    update

---
+++ resource/js/scripts.js
@@ -0,0 +1,14 @@
+funtion openSearch(evt, name) {
+    var i, tabContent, tablinks;
+    tabContent = document.getElementsByClassName("tabContent");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(name).style.display = "block";
+    evt.currentTarget.className += active;
+}
